7abd8c95098d8260ca9f598bef70ce652497670f
Georgii Krikun <georgii.krikun@gmail.com>
2026-02-27 13:06:17 +0100

    Basically arguments work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{ Parser,Subcommand, Arg, Command };
+use clap::{ Parser,Subcommand};
 use serde::Deserialize;
 use std::fs;
 use chrono::Utc;
@@ -30,7 +30,7 @@ struct Args {
 #[derive(Parser, Debug)]
 struct ConfigArgs {
     #[arg(short, long, required = true)]
-    input: String,
+    conf_file: String,
 }
 
 #[derive(Subcommand, Debug, Deserialize)]
@@ -78,36 +78,70 @@ enum ConfigParserError {
     
     #[error("TOML parsing error: {0}")]
     TomlError(#[from] toml::de::Error),
+    
+    #[error("Command-line parsing error: {0}")]
+    ClapError(#[from] clap::error::Error),
+    
+    #[error("Multiple errors occurred:\n{}", format_multiple(.0))]
+    Multiple(Vec<ConfigParserError>),
+}
+
+// Helper function to join the errors properly
+fn format_multiple(errors: &[ConfigParserError]) -> String {
+    errors.iter()
+        .map(|e| e.to_string()) // This calls the Display trait, which handles ANSI codes
+        .collect::<Vec<_>>()
+        .join("\n---\n") // Visual separator between errors
 }
 
 fn read_config<ArgsType>(config: &ConfigArgs) -> Result<ArgsType, ConfigParserError> 
 where ArgsType: for<'a> Deserialize<'a> {
     // Read the file content
-    let content = fs::read_to_string(&config.input)?;
+    let content = fs::read_to_string(&config.conf_file)?;
     
     // Parse the TOML into the Config struct
     let config: ArgsType = toml::from_str(&content)?;
     Ok(config)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::try_parse();
-    if let Err(e) = args {
-        let cfg_args = ConfigArgs::try_parse();
-        if let Err(cfg_e) = &cfg_args {
-            eprintln!("Error parsing command-line arguments: {}", e);
-            eprintln!("Error parsing config arguments: {}", cfg_e);
-            std::process::exit(1);
-        } else {
-            let cfg_args = cfg_args.as_ref().unwrap();
-            println!("Parsed config arguments: {:?}", cfg_args);
-            let config = read_config::<Args>(&cfg_args)?;
-            println!("Parsed config: {:?}", config);
-        }
-    } else {
-        let args = args.as_ref().unwrap();
-        println!("Parsed command-line arguments: {:?}", args);
-    }
+fn parse_config<ArgsType>() -> Result<ArgsType, ConfigParserError> 
+where ArgsType: for<'a> Deserialize<'a> + Parser + std::fmt::Debug
+{
+    let cli_args = ArgsType::try_parse()
+        .map_err(ConfigParserError::from);
+    let cfg_args = ConfigArgs::try_parse()
+        .map_err(ConfigParserError::from)
+        .and_then(|cfg_args| {
+            read_config::<ArgsType>(&cfg_args)
+        });
     
-    Ok(())
+    log::debug!("Parsed command-line arguments: {:?}", cli_args);
+    log::debug!("Parsed config arguments: {:?}", cfg_args);
+    
+    match (cli_args, cfg_args) {
+        (Ok(cli_args), Ok(_)) => Ok(cli_args),
+        (Ok(cli_args), Err(_)) => Ok(cli_args),
+        (Err(_), Ok(cfg_args)) => Ok(cfg_args),
+        (Err(cli_err), Err(cfg_err)) => {
+            let errors = vec![
+                cli_err,
+                cfg_err,
+            ];
+            Err(ConfigParserError::Multiple(errors))
+        }
+    }
+}
+
+fn main() {
+    env_logger::init();
+    let args = parse_config::<Args>();
+    match args {
+        Ok(args) => {
+            log::info!("Successfully parsed arguments: {:?}", args);
+        },
+        Err(e) => {
+            log::error!("Error parsing arguments: {}", e);
+            std::process::exit(1);
+        }
+    }
 }
